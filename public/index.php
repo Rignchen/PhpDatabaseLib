4134dd5ad0896c368904fa95412348922e89f2f2
Rignchen <rinchendemort@gmail.com>
2024-02-13 08:05:38 +0100

    idea: chose create table syntaxe

--- public/index.php
+++ public/index.php
@@ -3,3 +3,11 @@ require_once '../vendor/autoload.php';
 
 use Rignchen\Database\Database;
 
+$pdo = new PDO('sqlite:identifier.sqlite');
+$db = new Database($pdo);
+
+$db->createTable('identifier', [
+    'id' => 'INTEGER PRIMARY KEY',
+    'name' => 'TEXT',
+    'value' => 'TEXT'
+]);
